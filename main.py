4c2b97a140a388ade217210aaa8e6f3b90d4a88c
Zakariyya <91195283+ZZakariyya@users.noreply.github.com>
2024-01-30 16:47:11 +0400

    Add files via upload
    Added main file for app to run things, added requirements for the use of application and picture for the later use

---
+++ main.py
@@ -0,0 +1,12 @@
+from PySide6.QtWidgets import QApplication
+from app.main_window import MainWindow
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
